c6aa437f1fe423f5ce8e35f7d032814632335864
lozinski <lozinski@umich.edu>
2017-10-01 22:58:19 -0400

    First draft of first two exercises completed

--- headers/ex1/main.cpp
+++ headers/ex1/main.cpp
@@ -2,18 +2,23 @@
 #include "maths.h"
 using std::cout;
 
+
+/*
+ * DO NOT EDIT THIS MAIN FUNCTION
+ */
+
 int main(int argc, char* argv[])
 {
 	int res = add(10, 7);
 	int res2 = add(-5, 15);
 	int res3 = add(0, 0);
-	if (res == 17 && res2 == 10 && res3 == 0);
+	if (res == 17 && res2 == 10 && res3 == 0)
 	{
-		cout << "Good job, move onto the next exercise." << '\n';
+		cout << "Good job! Move onto the next exercise.\n";
 	}
 	else
 	{
-		cout << "You implemented add() incorrectly, go back and check." << '\n';
+		cout << "add() is not working as expected.\n";
 	}
 	
 	return 0;
--- headers/ex1/maths.h
+++ headers/ex1/maths.h
@@ -1,1 +1,13 @@
+#ifndef ADD_H
+#define ADD_H
+
+/*
+ * Don't worry about these statements beginning with '#'. 
+ * They will be explained soon enough.
+ */
+
+
+//DO NOT EDIT THIS FUNCTION DECLARATION
 int add(int x, int y);
+
+#endif
