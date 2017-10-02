6fa9ef3c6363564dad6ad6f772a9e5596ab8c179
lozinski <lozinski@umich.edu>
2017-10-02 18:44:46 -0400

    Added exercise 2

---
+++ headers/ex2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "maths.h"
+#include "maths.h"
+
+using std::cout;
+
+//DO NOT EDIT THIS MAIN FUNCTION
+int main(int argc, char* argv[])
+{
+	if(add(1,1) == 2)
+	{
+		cout << "Good job! Move onto the next exercise.\n";
+	}
+	else
+	{
+		cout << "add() is not working as expected.\n";
+	}
+}
